352f93513e5471006dbf4dced12a98de840ac0a3
Kacper Sierakowski <k.sierakowski@luca.eu>
2020-01-29 20:59:12 +0100

    Clean

--- Snickers/MainWindow.xaml.cs
+++ Snickers/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,54 @@ using System.Windows.Shapes;
 
 namespace Snickers
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private static TextBlock MyTextBlock = new TextBlock();
+        private static Button MyButton = new Button();
+        //private static bool IsSending = false;
         public MainWindow()
         {
             InitializeComponent();
+            MyTextBlock = textBlock;
+            MyButton = button;
+        }
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StartSending();
+        }
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            StopSending();
+        }
+        public delegate void UpdateTextCallback(string message);
+        private static Thread MyThread;
+        public void StartSending()
+        {
+            //IsSending = true;
+            MyThread = new Thread(SendData);
+            MyThread.Start();
+        }
+        static private void SendData()
+        {
+            int i = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                MyTextBlock.Dispatcher.Invoke(
+                    new UpdateTextCallback(UpdateText),
+                    new object[] { i.ToString() }
+                );
+                i++;
+            }
+        }
+        static private void UpdateText(string message)
+        {
+            MyTextBlock.Text=(message + "\n");
+        }
+        static private void StopSending()
+        {
+            //IsSending = false;
+            MyThread.Abort();
         }
     }
 }
